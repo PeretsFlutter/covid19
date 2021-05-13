2dde0856f5ccfa34fa9d2d1ed781cdc075cb8dfc
PeretsFlutter <ironmanbitcoinpro@gmail.com>
2021-05-13 15:29:41 +0300

    Init

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+final darkTheme = ThemeData(
+  primarySwatch: Colors.grey,
+  primaryColor: Colors.black,
+  brightness: Brightness.dark,
+  backgroundColor: const Color(0xFF212121),
+  accentColor: Colors.yellow[700],
+  accentIconTheme: IconThemeData(color: Colors.black),
+  dividerColor: Colors.black12,
+);
+
+final lightTheme = ThemeData(
+  primarySwatch: Colors.grey,
+  primaryColor: Colors.white,
+  brightness: Brightness.light,
+  backgroundColor: const Color(0xFFE5E5E5),
+  accentColor: Colors.orangeAccent,
+  accentIconTheme: IconThemeData(color: Colors.white),
+  dividerColor: Colors.white54,
+);
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      home: SplashScreen(
+        seconds: 3,
+        imageBackground: AssetImage('images/covidNew.png'),
+        loadingText: Text(
+          'Залишайтеся вдома та брежіть близьких',
+          style: TextStyle(fontFamily: 'MyFont', fontWeight: FontWeight.bold),
+        ),
+        loaderColor: Colors.orange,
+        navigateAfterSeconds: null,
+      ),
+    );
+  }
+}
