ef61c306c3e6fd802916fb041afa90def23bc204
PeretsFlutter <ironmanbitcoinpro@gmail.com>
2021-05-14 12:23:06 +0300

    MainImage

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -8,7 +8,12 @@ class Dashboard extends StatefulWidget {
 
 class _Dashboardstate extends State<Dashboard> with TickerProviderStateMixin {
   int currentIndex = 0;
-  final List<Widget> children = [];
+  final List<Widget> children = [
+    Text('Global'),
+    Text('Ukraine'),
+    Text('World'),
+    Text('About'),
+  ];
   AnimationController _controller;
 
   @override
@@ -57,10 +62,110 @@ class _Dashboardstate extends State<Dashboard> with TickerProviderStateMixin {
               'Світ',
               style: TextStyle(fontFamily: 'MyFont'),
             ),
+          ),
+          BottomNavigationBarItem(
+            activeIcon: RotationTransition(
+              turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+              child: Icon(Icons.star),
+            ),
+            icon: Icon(Icons.star),
+            title: Text(
+              'Україна',
+              style: TextStyle(fontFamily: 'MyFont'),
+            ),
+          ),
+          BottomNavigationBarItem(
+            activeIcon: RotationTransition(
+              turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+              child: Icon(Icons.flag),
+            ),
+            icon: Icon(Icons.flag),
+            title: Text(
+              'Країни',
+              style: TextStyle(fontFamily: 'MyFont'),
+            ),
+          ),
+          BottomNavigationBarItem(
+            activeIcon: RotationTransition(
+              turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+              child: Icon(Icons.info),
+            ),
+            icon: Icon(Icons.info),
+            title: Text(
+              'Автор',
+              style: TextStyle(fontFamily: 'MyFont'),
+            ),
+          ),
+        ],
+      ),
+      body: currentIndex == 0
+          ? SafeArea(
+              child: Scaffold(
+                body: ListView(
+                  children: [
+                    MainImage(),
+                    SizedBox(height: height * 0.035),
+                    children[currentIndex]
+                  ],
+                ),
+              ),
+            )
+          : children[currentIndex],
+    );
+  }
+}
+
+class MainImage extends StatefulWidget {
+  @override
+  _MainImageState createState() => _MainImageState();
+}
+
+class _MainImageState extends State<MainImage>
+    with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController _animationController;
+
+  toggleAnimation() {
+    animation = Tween(begin: 0.0, end: 25.0).animate(_animationController);
+    if (_animationController.isDismissed) {
+      _animationController.forward().whenComplete(() => toggleAnimation());
+    }
+    if (_animationController.isCompleted) {
+      _animationController.reverse().whenComplete(() => toggleAnimation());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 3000))
+          ..addListener(() => setState(() {}));
+    toggleAnimation();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(0, animation.value),
+      child: Column(
+        children: [
+          Image.asset(
+            'images/personFighting.png',
+            height: MediaQuery.of(context).size.height * 0.25,
+          ),
+          Text(
+            'Залишайтеся вдома, бережіть себе та інших',
+            style: TextStyle(fontWeight: FontWeight.bold),
           )
         ],
       ),
-      body: Text('ghkdhkg'),
     );
   }
 }
